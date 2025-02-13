49634c1e61a54ffbfafd668a1071a77c41e6e51f
reiken <ultee529@outlook.jp>
2025-02-14 07:03:38 +0900

    タイルの種類から位置、位置からタイルの種類を出力するメソッドの分離

--- src/utils/tileUtils.ts
+++ src/utils/tileUtils.ts
@@ -1,10 +1,10 @@
-export const tileIdToPosition = (tileId: number) => {
-  const col = Math.floor((tileId - 1) / 9) + 1;
-  const row = "ABCDEFGHI"[(tileId - 1) % 9];
+export const tileKindToPosition = (tileKind: number) => {
+  const col = Math.floor((tileKind - 1) / 9) + 1;
+  const row = "ABCDEFGHI"[(tileKind - 1) % 9];
   return { col, row };
 };
 
-export const positionToTileId = (col: number, row: string) => {
+export const positionToTileKind = (col: number, row: string) => {
   const rowIndex = "ABCDEFGHI".indexOf(row);
   return (col - 1) * 9 + rowIndex + 1;
 };
